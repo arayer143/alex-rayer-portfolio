2cbdfddef61ab728ca0c24cc631fa3a90d21c199
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-04-21 22:17:25 -0500

    update to the hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function HomePage(): JSX.Element {
 						}}
 						className="text-gray-500 dark:text-white text-5xl sm:text-6xl md:text-6xl lg:text-8xl tracking-tight font-extrabold">
 						Hey <span className="inline-block origin-70 hover:(animate-wave)">👋</span>{' '}
-						I&apos;m Ben, <br className="hidden sm:block" />a{' '}
+						I am Alex Rayer, <br className="hidden sm:block" />a{' '}
 						<Pill.Standard className="mt-4">developer</Pill.Standard>
 					</Animate>
 
