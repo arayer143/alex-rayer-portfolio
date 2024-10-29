b8c71b9df526759cff059bc7961a058ed60b122b
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-10-29 11:11:41 -0500

    removed all the configuration for the docker container that we were experimenting with

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/contact',
-        destination: 'http://localhost:8000/contact.php',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
